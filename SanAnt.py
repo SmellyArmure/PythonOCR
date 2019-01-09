c3a3dcdc6fb02f9394476e5c3c739fe22d9b1288
Maryse <muller.maryse@gmail.com>
2019-01-09 20:39:36 +0100

    Ajouté générateur de citations au hasard avec interaction de l'utilisateur

--- SanAnt.py
+++ SanAnt.py
@@ -1,10 +1,12 @@
-# commentaires préliminaires branche 01
-# Ceci est un commentaire dans la branche Master
+# 01 - Utiliser les comparateurs
 # -*- coding: utf8 -*-
 import random
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre.",
+    "Il était une fois une princesse...",
+    "C'est la mère Michel qui a perdu son chat",
+    "Allons enfants de la patriiiie, le jour de gloire est arrivé",
 ]
 
 characters = [
@@ -17,3 +19,13 @@ characters = [
     "Kirikou"
 ]
 print("Bonjour à vous," + characters[random.randint(0,len(characters)-1)] + ".")
+
+rep = "\n"
+while rep!="B":
+	rep=input("Taper entrée pour une nouvelle citation ou B pour quitter :  \n")
+	if rep == "":
+		print(quotes[random.randint(0, len(quotes)-1)])
+	else:
+		"--> mauvaise réponse...on reboucle"
+print("Merci, au-revoir")
+input("--------FIN--------")
